70f9477278857793b84ce06b36fb3404271b113d
Miguel Angel Suarez Montiel <mi.angel.itics@gmail.com>
2017-09-17 22:13:21 -0500

    Se agrega mongoose para manejo de datos

--- api-restful-favoritos/index.js
+++ api-restful-favoritos/index.js
@@ -1,9 +1,18 @@
 'use strict'
 
+var mongoose = require('mongoose');
 var app = require('./app');
 var port = process.env.port || 3678;
 
-app.listen(port, () => {
-    console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
-    console.log("otro console para validar el cambio automatico con el nodemon");
+mongoose.connect('mongodb://localhost:27017/cursofavoritos', (err, res) => {
+    if (err) {
+        throw err;
+    } else {
+        console.log("Conexión a mongodb correcta");
+        app.listen(port, () => {
+            console.log(`API REST FAVORITOS funcionando en http://localhost:${port}`);
+            console.log("otro console para validar el cambio automatico con el nodemon");
+        });
+    }
+
 });
